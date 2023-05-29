b3a7730a9cf27c87af1ea14d79bdd7eed1f8a7e9
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2023-05-30 00:33:35 +0200

    Add relative assets

--- config.rb
+++ config.rb
@@ -1,6 +1,9 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
+activate :relative_assets
+set :relative_links, true
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
